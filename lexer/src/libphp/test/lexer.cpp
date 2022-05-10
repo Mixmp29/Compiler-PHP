8e5755fea138f2780f2ecf3eeba22e433a49394a
Mixmp29 <denisov.mihail2013@yandex.ru>
2022-05-10 10:08:10 +0700

    Add tests on numbers and symbols

--- lexer/src/libphp/test/lexer.cpp
+++ lexer/src/libphp/test/lexer.cpp
@@ -1,9 +1,45 @@
 #include <gtest/gtest.h>
 
+#include <libphp/dump_tokens.hpp>
+
 #include <sstream>
 #include <string>
 
-TEST(LexerSuite, ValidNumbers) {
-      EXPECT_EQ(42 + 1, 43);
+TEST(LexerSuite, JustTest) {
+  EXPECT_EQ(42 + 1, 43);
 }
 
+namespace php::test {
+
+TEST(LexerSuite, ValidNumbers) {
+  // Given
+  std::stringstream in("0 123456789");
+
+  // When
+  std::stringstream out;
+  dump_tokens(in, out);
+
+  // Then
+  EXPECT_EQ(
+      out.str(),
+      "Loc=<1:0>\tValue '0'\n"
+      "Loc=<1:2>\tValue '123456789'\n");
+}
+
+TEST(LexerSuite, ValidId) {
+  // Given
+  std::stringstream in("a bNFDS fsddf");
+
+  // When
+  std::stringstream out;
+  dump_tokens(in, out);
+
+  // Then
+  EXPECT_EQ(
+      out.str(),
+      "Loc=<1:0>\tId 'a'\n"
+      "Loc=<1:2>\tId 'bNFDS'\n"
+      "Loc=<1:8>\tId 'fsddf'\n");
+}
+
+}  // namespace php::test
