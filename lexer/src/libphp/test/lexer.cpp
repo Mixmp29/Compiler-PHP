789a4ac5fcb5da3112090614ff8f112adbb4b3e7
Mixmp29 <denisov.mihail2013@yandex.ru>
2022-05-10 11:15:24 +0700

    Fix commit Foreach

--- lexer/src/libphp/test/lexer.cpp
+++ lexer/src/libphp/test/lexer.cpp
@@ -72,4 +72,36 @@ TEST(LexerSuite, Foreach) {
   std::stringstream out;
   dump_tokens(in, out);
 
+  EXPECT_EQ(
+      out.str(),
+      "Loc=<1:0>	Foreach 'foreach'\n"
+      "Loc=<1:8>	L_Paren '('\n"
+      "Loc=<1:9>	Dollar '$'\n"
+      "Loc=<1:10>	Id 'arr'\n"
+      "Loc=<1:14>	As 'as'\n"
+      "Loc=<1:17>	Error_Character '&'\n"
+      "Loc=<1:18>	Dollar '$'\n"
+      "Loc=<1:19>	Id 'value'\n"
+      "Loc=<1:24>	R_Paren ')'\n"
+      "Loc=<1:26>	L_Brace '{'\n"
+      "Loc=<2:4>	If 'if'\n"
+      "Loc=<2:7>	L_Paren '('\n"
+      "Loc=<2:8>	Dollar '$'\n"
+      "Loc=<2:9>	Id 'min'\n"
+      "Loc=<2:13>	Sharp_Brackets_Right '>'\n"
+      "Loc=<2:15>	Dollar '$'\n"
+      "Loc=<2:16>	Id 'value'\n"
+      "Loc=<2:21>	R_Paren ')'\n"
+      "Loc=<2:23>	Colon ':'\n"
+      "Loc=<3:8>	Dollar '$'\n"
+      "Loc=<3:9>	Id 'min'\n"
+      "Loc=<3:13>	Equality '='\n"
+      "Loc=<3:15>	Dollar '$'\n"
+      "Loc=<3:16>	Id 'value'\n"
+      "Loc=<3:21>	Semicolon ';'\n"
+      "Loc=<4:4>	Endif 'endif'\n"
+      "Loc=<4:9>	Semicolon ';'\n"
+      "Loc=<5:0>	R_Brace '}'\n");
+}
+
 }  // namespace php::test
